bcd2e24efee30a338c087f9eb433376ad1c2cf51
Lizzyfxjk <lizmarisolg@nycstudents.net>
2025-09-15 16:10:15 +0000

    finished task

---
+++ Java-3-Activity/Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("Liz Guallpa");
+   System.out.println("Liz Guallpa\n108-22 38th Ave");
+    
+  }
+
+
+}
