96f56a3759736906d4cd0e71ff15f29c2665033f
Lizzyfxjk <lizmarisolg@nycstudents.net>
2025-09-18 04:32:09 +0000

    finished task!

--- Java-4-Discussion/Main.java
+++ Java-4-Discussion/Main.java
@@ -9,16 +9,22 @@ class Main {
   void init(){
     // Number of cars
        int numOfCars = 20;
+       System.out.println("number of cars" + numOfCars);
     // Investment amount in currency
        double invstmtAmt = 200.50;
+       System.out.println("Investments amount in currency" + invstmtAmt);
     // Person's first name
-       String fName = "Mary";
+       String name = "Mary";
+       System.out.println("Person's first name" + name);
     // Is a game over?
        boolean isGameOver = false;
+       System.out.println("Is game over" + isGameOver);
     // Store the first initial of a name
-       char fInitial = 'N';
+       char Initial = 'N';
+       System.out.println("Store the first initial" + Initial);
     // Maximum number of customers in a theater (declare as a constant)
        final int MaxCust = 500;
+       System.out.println("Maximum number of customers" + MaxCust);
 
   }
 
