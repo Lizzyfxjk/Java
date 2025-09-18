d4de4b6aa38a3230af0023da4d1df412a2dce44b
Lizzyfxjk <lizmarisolg@nycstudents.net>
2025-09-18 03:03:37 +0000

    finished task

--- Java-3b-CW/Main.java
+++ Java-3b-CW/Main.java
@@ -11,22 +11,21 @@ class Main {
   */
   void init(){
     // print Welcome
-    
+    System.out.println("Welcome");
 
     // print Hello with the quotes displayed
-    
-    
+    System.out.println("\"Hello\"");
     // print \Java Code\ as shown
-    
+    System.out.println("\\Java Code\\");
     
     // print the name Joe tabbed once
-    
+    System.out.println("Joe\t");
 
     // print the name Mary tabbed twice
-    
+    System.out.println("Joe\tJoe");
     
     // print one, two and three on separate lines using a single line of code
-      
+      System.out.println("one\ntwo\nthree");
 	
   }
   
